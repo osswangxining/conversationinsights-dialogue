e5e83de03d7bda65518f70791589366864af1a2e
osswangxining <osswangxining@163.com>
2017-10-08 14:57:18 +0800

    conversation

--- conversationinsights/conversation.py
+++ conversationinsights/conversation.py
@@ -21,8 +21,8 @@ class Dialogue(object):
         self.events = events
 
     def __str__(self):
-        return "Dialogue with name '{}' and turns:\n{}".format(self.name,
-                                                               "\n\n".join(["\t{}".format(t) for t in self.events]))
+        return "Dialogue with name '{}' and turns:\n{}".format(
+                self.name, "\n\n".join(["\t{}".format(t) for t in self.events]))
 
 
 class Topic(object):
@@ -30,3 +30,11 @@ class Topic(object):
 
     def __init__(self, name):
         self.name = name
+
+
+# The default topic will not carry a name nor will it overwrite the topic of
+# a dialog e.g. if an action of this default topic is executed, the previous
+# topic is kept active
+DefaultTopic = Topic(None)
+
+QuestionTopic = Topic("question")
